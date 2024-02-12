6c83e65f7129656b0ded7b5f773939cedbe82ff6
Mégane Navarro <mnavarro@live.fr>
2024-02-12 14:55:17 +0100

    Affichage des recettes d'après les données

---
+++ scripts/index.js
@@ -0,0 +1,64 @@
+async function init() {
+    recipes.forEach((recipe) => {
+        createRecipe(recipe);
+    });
+}
+
+window.onload = (event) => {
+    init();
+};
+
+async function createRecipe(recipe) {
+    const recipesSection = document.querySelector(".recipes");
+    const article = document.createElement("article");
+    article.classList.add("recipe-block");
+    const imgRecipe = document.createElement("img");
+    imgRecipe.setAttribute("src", "./assets/photos/" + recipe["image"]);
+    const divDuration = document.createElement("div");
+    divDuration.classList.add("duration");
+    divDuration.textContent = recipe["time"] + "min";
+    const divRecipe = document.createElement("div");
+    divRecipe.classList.add("container-recipe");
+    const titleRecipe = document.createElement("h1");
+    titleRecipe.textContent = recipe["name"];
+    const h2Recipe = document.createElement("h2");
+    h2Recipe.textContent = "Recette";
+    const pRecipe = document.createElement("p");
+    pRecipe.classList.add("recipe-text");
+    pRecipe.textContent = recipe["description"];
+    const h2Ingredients = document.createElement("h2");
+    h2Ingredients.textContent = "Ingrédients";
+    const divIngredients = document.createElement("div");
+    divIngredients.classList.add("block-ingredients");
+
+    recipesSection.appendChild(article);
+    article.appendChild(imgRecipe);
+    article.appendChild(divDuration);
+    article.appendChild(divRecipe);
+    divRecipe.appendChild(titleRecipe);
+    divRecipe.appendChild(h2Recipe);
+    divRecipe.appendChild(pRecipe);
+    divRecipe.appendChild(h2Ingredients);
+    divRecipe.appendChild(divIngredients);
+
+    const ingredientsArray = recipe["ingredients"];
+    ingredientsArray.forEach((ingredient) => {
+        const ul = document.createElement("ul");
+        const liIngredients = document.createElement("li");
+        const pIngredients = document.createElement("p");
+        const pQuantity = document.createElement("p");
+        pIngredients.classList.add("ingredients");
+        pIngredients.textContent = ingredient["ingredient"];
+        pQuantity.classList.add("quantity");
+        if(ingredient["quantity"]) {
+            pQuantity.textContent = ingredient["quantity"];
+        }
+        if(ingredient["unit"]) {
+            pQuantity.textContent += " " + ingredient["unit"];
+        }
+        divIngredients.appendChild(ul);
+        ul.appendChild(liIngredients);
+        liIngredients.appendChild(pIngredients);
+        liIngredients.appendChild(pQuantity);
+    });
+}
